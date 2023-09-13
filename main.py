6e32d7b3d2b23dde9debefe6ed4867556043de47
leandropclemente <144954141+leandropclemente@users.noreply.github.com>
2023-09-13 16:55:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#1 crie um sistema que some 2 numeros:
+
+numero = 10
+numero2 = 55
+print(numero + numero2)
+
+
+#2 crie um sistema que imprima seu nome na tela
+
+nome =  ("leandro")
+print (nome)
+
+#3 crie um sistema que imprima seu nome e um cumprimento apos o nome:
+
+text = input ("digite um cumprimento")
+nome = input ("digite seu nome - ")
+print (text, "ola tudo bem?")
+
+#4 crie um sistema para multiplicar dois numeros 
+
+numero = 10
+numero2 = 55
+print(numero * numero2)
